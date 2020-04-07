5ad27eb3dbf0c15d4aba3e14e50272eb295b13a9
Marina Svistkova <svmrin1@gmail.com>
2020-04-07 18:11:44 +0300

    Initial Commit

---
+++ WeatherCast/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherCast
+//
+//  Created by Marina Svistkova on 07.04.2020.
+//  Copyright © 2020 Marina Svistkova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
